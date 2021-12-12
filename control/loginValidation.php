b226436aab10367c6238cc841eb2d9f34fdc12f6
Kibria10 <junakkibria56@gmail.com>
2021-12-12 12:00:37 +0600

    patient login-reg validation added

--- control/loginValidation.php
+++ control/loginValidation.php
@@ -15,17 +15,17 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 // $message = $_POST['message'];
 
-$s = "select * from admindata where email = '$email' && password = '$password' ";
+$s = "select * from patientdata where email = '$email' && password = '$password' ";
 $result = mysqli_query($conn, $s);
 $num = mysqli_num_rows($result);
 
 if($num == 1){
  $_SESSION['email'] = $email;
-   header('location:admin.php');
+   header('location:home.php');
 
 }
 else{
-    header('location:error.php');
+    header('location:register.php');
 }
 
 ?>
